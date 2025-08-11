148c24d3b70496703619dab28d67dde4edff04d3
Poorvi Gupta <liliput011199@gmail.com>
2025-08-11 22:04:05 +0530

    update application model

--- server/models/Application.js
+++ server/models/Application.js
@@ -1,30 +1,28 @@
-const mongoose = require("mongoose");
-
 const applicationSchema = new mongoose.Schema(
- {
-   job:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref: "Job",
-    required: true,
-   },
-   applicant:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required:true,
-   },
-   status:{
-    type:String,
-    enum: ["pending", "accepted", "rejected"],
-    default:"pending",
-   },
-   appliedAt: {
-    type:Date,
-    default: Date.now,
-   },
- },
-   {
-    timestamps:true
-   },
+  {
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+    },
+    applicant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    resume: { 
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+    appliedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
 );
-
-module.exports = mongoose.model("Application",applicationSchema);
