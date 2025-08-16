b6057a1ef23911a2119b5f8f820817e9172e9604
Poorvi Gupta <liliput011199@gmail.com>
2025-08-16 22:45:51 +0530

    update authcontroller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -65,7 +65,7 @@ exports.getUserProfile = async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    res.json(user);
+    res.json({user});
   } catch (err) {
     console.error("Error fetching user profile:", err);
     res.status(500).json({ error: "Server error" });
